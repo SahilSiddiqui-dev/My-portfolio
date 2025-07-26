674a29e1ad65838a5858d828891fedb75af10fe6
Mohd Sahil <sahilsiddiqui2424@gmail.com>
2025-07-27 00:16:42 +0530

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -40,7 +40,7 @@ export default function Header() {
           {navItems.map((item, index) => (
             <motion.div
               key={item.name}
-              initial={{ opacity: 0, y: -20 }}
+              initial={{ opacity: 0, y: -10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.2, delay: index * 0.05 }}
             >
